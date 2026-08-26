3f3e7eceb443b9d631912a7811d6477af731bccc
Jeremy Odell <jeremyodell@station-mini.lan>
2026-08-25 20:04:53 -0400

    chore(release): prepare pre-alpha.6

--- apps/cli/test/integration/manual-smoke-commands.test.ts
+++ apps/cli/test/integration/manual-smoke-commands.test.ts
@@ -136,7 +136,7 @@ describe("CLI manual-smoke commands", () => {
       "--version",
     ]);
 
-    expect(direct).toEqual({ code: 0, output: "0.0.0-pre-alpha.5.16", outputFormat: "text" });
+    expect(direct).toEqual({ code: 0, output: "0.0.0-pre-alpha.6", outputFormat: "text" });
     expect(withMissingConfig).toEqual(direct);
   });
 
